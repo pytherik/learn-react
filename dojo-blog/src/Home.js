0ae0027b9a0d6c54c6e0911d841381219beb3338
pytherik <erikberndt@gmx.net>
2023-07-09 19:12:06 +0200

    handleDelete not working

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState ([
+    {title: 'Meine neue Website', body: 'lorem ipsulm...', author: 'erik', id: 1},
+    {title: 'Welcome to the party!', body: 'lorem ipsulm...', author: 'rolf', id: 2},
+    {title: 'Web Dev Top Tips', body: 'lorem ipsulm...', author: 'hansi', id: 3}
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter(blog => blog.author === 'erik')}
+                title="Erik's blogs"
+                handleDelete={handleDelete}/>
     </div>
   );
 };
