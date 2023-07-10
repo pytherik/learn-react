67645ccf33822fff38e9fe4e468261f309650a26
pytherik <erikberndt@gmx.net>
2023-07-10 17:31:17 +0200

    create react-task-tracker

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,19 +8,15 @@ const Home = () => {
     {title: 'Web Dev Top Tips', body: 'lorem ipsulm...', author: 'hansi', id: 3}
   ]);
 
-  const [greeting, changeGreeting] = useState('Hello');
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
-  const handleGreeting = (g) => changeGreeting(g);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-      <p>{greeting}</p>
-      <button onClick={() => handleGreeting('Ciao!')}>greet me</button>
     </div>
 
   );
