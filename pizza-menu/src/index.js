47182f63842f20d337c7fac44a8b88bb14e792e1
pytherik <erikberndt@gmx.net>
2023-07-13 19:30:13 +0200

    follow pizza tut

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,17 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Fast Pizza Shit</h1>
+    </header>
+  )
+}
+
+const Pizza = ({ pizzaObj }) => {
+  return (
+    <div className="pizza">
+      <img src={ pizzaObj.photoName } alt={pizzaObj.name}/>
+      <li>
+        <h3>{ pizzaObj.name }</h3>
+        <p>{ pizzaObj.ingredients }</p>
+        <span>{ pizzaObj.price }</span>
+      </li>
+    </div>
+  )
+}
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h2>Our Angebot</h2>
+      <ul className="pizzas">
+        {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} /> )}
+      </ul>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return <footer className="footer">{new Date().toLocaleTimeString()} We're currently frustrated</footer>
+}
+
+
+const  App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
